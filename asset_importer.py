af411d962a4c669d7cd4036ba2caa2ea63096f09
radianceeeeeeee <97012089+radianceeeeeeee@users.noreply.github.com>
2023-07-18 22:10:03 +0800

    Moved the functions into a class

--- asset_importer.py
+++ asset_importer.py
@@ -4,67 +4,83 @@ from tkinter import filedialog, simpledialog, messagebox
 from tkinter import END
 import tkinter as tk
 from enum import Enum
+from PIL import ImageTk, Image
 
-HEIGHT_WINDOW = 400
-WIDTH_WINDOW = 500
+HEIGHT_WINDOW = 600
+WIDTH_WINDOW = 800
 
 class AssetTag(str, Enum):
     NAME = "name"
     EDITOR_IMAGE = "image"
     EXT_SETTINGS = "extra-settings"
+    ICON = "icon"
+
+class Extension(str, Enum):
+    INI = "ini"
+    IMAGE = "png"
+    LUA = "lua"
+    JSON = "json"
 
 class Session():
     def __init__(self):
+        self.assets = []
+
+
+    def find_asset_info(self, luaName, luaPath, assetTag):
+        iniFilePart = luaName.split(".")[0] + ".ini"
+        iniFile = os.path.join(luaPath, iniFilePart)
+        
+        if os.path.exists(iniFile):
+            lines = open(iniFile, "r").readlines()
+                
+            for l in lines:
+                parsed = l.split("=")
+                if parsed[0].strip() == assetTag:
+                    return parsed[1].strip()[1:-1] # strip removes whitespace, [1:-1] removes quotation marks
+            return None
+        else:
+            return None
+
+    def filter_asset(self):
         pass
 
+    def initialize_asset(self):
+        dir = filedialog.askdirectory()
+        items = os.listdir(dir)
+        entries = [f for f in items if f.endswith(".lua")]
 
-def find_asset(luaName, luaPath, assetTag):
-    iniFilePart = luaName.split(".")[0] + ".ini"
-    iniFile = os.path.join(luaPath, iniFilePart)
-    
-    if os.path.exists(iniFile):
-        lines = open(iniFile, "r").readlines()
-            
-        for l in lines:
-            parsed = l.split("=")
-            if parsed[0].strip() == assetTag:
-                return parsed[1].strip()[1:-1] # strip removes whitespace, [1:-1] removes quotation marks
-        return None
-    else:
-        return None
+        fileList.delete(0, END)
 
-def initialize_asset():
-    global importButtonState
-    dir = filedialog.askdirectory()
-    items = os.listdir(dir)
-    entries = [f for f in items if f.endswith(".lua")]
+        if len(entries) > 0:
+            importButton.configure(state = tk.NORMAL)
+            for e in entries:
+                current_asset = self.find_asset_info(e, dir, AssetTag.NAME)
 
-    fileList.delete(0, END)
+                if current_asset != None:
+                    fileList.insert(END, current_asset)
 
-    if len(entries) > 0:
-        importButton.configure(state = tk.NORMAL)
-        for e in entries:
-            current_asset = find_asset(e, dir, AssetTag.NAME)
-
-            if current_asset != None:
-                fileList.insert(END, current_asset)
-    else:
-        # do something
-        pass
+        folderName.configure(text = dir, foreground = "black")
+        #fileList.config(yscrollcommand = fileScroll.set)
+        #fileScroll.config(command = fileList.yview)
 
 
-def import_asset():
-    file = filedialog.askopenfilename(filetypes=[("Lua files", ".lua")])
-    filepath = os.path.dirname(file)
-    filename = os.path.basename(file)
+    def copy_asset(self, srcPath, dstPath, fileExt):
+        shutil.copyfile(srcPath, dstPath)
 
-    new_asset = find_asset(filename, filepath, AssetTag.NAME)
+    def import_asset(self):
+        file = filedialog.askopenfilename(filetypes=[("Lua files", ".lua")])
+        filepath = os.path.dirname(file)
+        filename = os.path.basename(file)
 
-    if new_asset != None:
-        fileList.insert(END, new_asset)
-    else:
-        messagebox.showerror(title = "Invalid Asset", message = "The asset you have chosen is invalid.")
+        new_asset = self.find_asset_info(filename, filepath, AssetTag.NAME)
 
+        if new_asset != None:
+            fileList.insert(END, new_asset)
+
+            # copying
+            # copy_assets(file, folderName["text"])]
+        else:
+            messagebox.showerror(title = "Invalid Asset", message = "The asset you have chosen is invalid.")
 
 
 # main program
@@ -73,20 +89,43 @@ if __name__ == "__main__":
     root.title("Asset Importer")
     root.resizable = False
 
-    canvas = tk.Canvas(root, width = WIDTH_WINDOW, height = HEIGHT_WINDOW)
+    #root.geometry(f"{WIDTH_WINDOW}x{HEIGHT_WINDOW}")
+
+    session = Session()
 
     importContainer = tk.LabelFrame(root, text = "Files")
-    openButton = tk.Button(importContainer, text = "Open level folder", command = initialize_asset)
-    importButton = tk.Button(importContainer, text = "Import new asset", command = import_asset, state = tk.DISABLED)
+    openButton = tk.Button(importContainer, text = "Open level folder", command = session.initialize_asset)
+    importButton = tk.Button(importContainer, text = "Import new asset", command = session.import_asset, state = tk.DISABLED)
 
     importContainer.grid(row = 0, column = 0, padx = 20, pady = 20)
     openButton.grid(row = 1, column = 0, padx = 10, pady = 10)
     importButton.grid(row = 1, column = 1, padx = 10, pady = 10)
 
     filesContainer = tk.LabelFrame(root, text = "Custom Assets in Folder")
-    filesContainer.grid(row = 2, column = 0, padx = 20, pady = 20)
+    filesContainer.grid(row = 2, column = 0, padx = 20, pady = 20, )
 
-    fileList = tk.Listbox(filesContainer)
-    fileList.grid(row = 3, column = 0, padx = 10, pady = 10)
+    folderContainer = tk.LabelFrame(filesContainer, text = "Current Assets")
+    folderContainer.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+    #folderScroll = tk.Scrollbar(folderContainer, orient = "horizontal")
+    #folderScroll.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+    folderName = tk.Label(filesContainer, text = "Pick a directory first", foreground = "gray")
+    folderName.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+    #fileScroll = tk.Scrollbar(folderContainer, orient = tk.VERTICAL, )
+    #fileScroll.grid(row = 4, column = 1)
+
+    fileList = tk.Listbox(folderContainer)
+    fileList.grid(row = 4, column = 0, padx = 10, pady = 10)
+
+    imageContainer = tk.LabelFrame(filesContainer, text = "Preview Image")
+    imageContainer.grid(row = 3, column = 1, padx = 10, pady = 10)
+
+    #imageDummy = Image.open("empty.png")
+    #imageContent = ImageTk.PhotoImage(imageDummy)
+
+    imagePrev = tk.Label(imageContainer, text = "Image")#, image = imageContent)
+    imagePrev.grid(row = 4, column = 1, padx = 10, pady = 10)
 
     root.mainloop()
